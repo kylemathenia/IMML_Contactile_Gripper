42494eb3d04f87ae778247cbe56e2e138d00a231
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-03-09 23:42:07 -0800

    Initial viz script

--- ROS_Workspace/src/contactile_gripper/bag/get_csv_data.py
+++ ROS_Workspace/src/contactile_gripper/bag/get_csv_data.py
@@ -1,13 +1,43 @@
 
-
-# from bagpy import bagreader
 import pandas as pd
 import numpy as np
-# b = bagreader('grasp_and_release_2022-03-09-21-39-10.bag')
-# # replace the topic name as per your need
-# name_of_csv_file_written = b.message_by_topic(['/hub_0/sensor_0'])
-# print(name_of_csv_file_written)
-name_of_csv_file_written = 'hub_0-sensor_0.csv'
-data = pd.read_csv(name_of_csv_file_written)
-my_data = np.genfromtxt(name_of_csv_file_written, delimiter=',')
-data # prints laser data in the form of pandas dataframe
+frames=[]
+# 1st dim == frame num, 2nd dim == xforce grid(0), yforce grid(1), zforce grid(2)
+# Each grid is 2 dim array. 1st dim is row, 2nd dim is column of pillars.
+
+def get_grids(pillar_data):
+    xgrid=np.empty(0)
+    ygrid=np.empty(0)
+    zgrid=np.empty(0)
+    for i, pillar in enumerate(pillar_data):
+        pillar_list = pillar.split('\r\n')
+        fx = pillar_list[10].split(':')
+        fx = float(fx[1])
+        fy = pillar_list[11].split(':')
+        fy = float(fy[1])
+        fz = pillar_list[12].split(':')
+        fz = float(fz[1])
+        xgrid = np.append(xgrid,fx)
+        ygrid = np.append(ygrid,fy)
+        zgrid = np.append(zgrid,fz)
+    xgrid = list(xgrid.reshape(3,3))
+    ygrid = list(ygrid.reshape(3, 3))
+    zgrid = list(zgrid.reshape(3, 3))
+    return xgrid,ygrid,zgrid
+
+def main():
+    name_of_csv_file_written = 'hub_0-sensor_0.csv'
+    dataframe = pd.read_csv(name_of_csv_file_written)
+    data = dataframe.to_numpy()
+    for sample in data:
+        pillar_data = sample[6]
+        pillar_data = pillar_data.strip('[]')
+        pillar_data = pillar_data.split(',')
+        xgrid, ygrid, zgrid = get_grids(pillar_data)
+        frames.append([xgrid, ygrid, zgrid])
+    return frames
+    print(frames)
+
+
+if __name__ == "__main__":
+    main()
--- ROS_Workspace/src/contactile_gripper/bag/viz_test.py
+++ ROS_Workspace/src/contactile_gripper/bag/viz_test.py
@@ -1,8 +1,10 @@
-import numpy as np
+
 from matplotlib import pyplot as plt
 from matplotlib import animation
 from matplotlib.colors import ListedColormap
 import matplotlib
+import pandas as pd
+import numpy as np
 
 
 class SensorViz:
@@ -46,5 +48,41 @@ class SensorViz:
 
         return Q,
 
+def data_prep():
+    frames = []
+    # 1st dim == frame num, 2nd dim == xforce grid(0), yforce grid(1), zforce grid(2)
+    # Each grid is 2 dim array. 1st dim is row, 2nd dim is column of pillars.
+    name_of_csv_file_written = 'hub_0-sensor_0.csv'
+    dataframe = pd.read_csv(name_of_csv_file_written)
+    data = dataframe.to_numpy()
+    for sample in data:
+        pillar_data = sample[6]
+        pillar_data = pillar_data.strip('[]')
+        pillar_data = pillar_data.split(',')
+        xgrid, ygrid, zgrid = get_grids(pillar_data)
+        frames.append([xgrid, ygrid, zgrid])
+    return frames
+
+def get_grids(pillar_data):
+    xgrid = np.empty(0)
+    ygrid = np.empty(0)
+    zgrid = np.empty(0)
+    for i, pillar in enumerate(pillar_data):
+        pillar_list = pillar.split('\r\n')
+        fx = pillar_list[10].split(':')
+        fx = float(fx[1])
+        fy = pillar_list[11].split(':')
+        fy = float(fy[1])
+        fz = pillar_list[12].split(':')
+        fz = float(fz[1])
+        xgrid = np.append(xgrid, fx)
+        ygrid = np.append(ygrid, fy)
+        zgrid = np.append(zgrid, fz)
+    xgrid = list(xgrid.reshape(3, 3))
+    ygrid = list(ygrid.reshape(3, 3))
+    zgrid = list(zgrid.reshape(3, 3))
+    return xgrid, ygrid, zgrid
+
 if __name__ == "__main__":
-    _ = SensorViz(data=None,filename="quiver")
+    data=data_prep()
+    _ = SensorViz(data=data,filename="quiver_test")
