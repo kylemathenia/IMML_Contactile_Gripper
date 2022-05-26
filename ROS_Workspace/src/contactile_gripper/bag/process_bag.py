7a2a035b1a91a8c76da1f11ccf3dbf023f1be90b
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-05-25 20:51:37 -0700

    Created pose node

--- ROS_Workspace/src/contactile_gripper/bag/process_bag.py
+++
@@ -1,8 +0,0 @@
-from bagpy import bagreader
-import pandas as pd
-b = bagreader('grasp_and_release_2022-03-10-12-14-33.bag')
-# replace the topic name as per your need
-name_of_csv_file = b.message_by_topic(['/hub_0/sensor_0'])
-print(name_of_csv_file)
-dataframe = pd.read_csv(name_of_csv_file)
-dataframe # prints laser data in the form of pandas dataframe
--- ROS_Workspace/src/contactile_gripper/support/pose_models.py
+++ ROS_Workspace/src/contactile_gripper/support/pose_models.py
@@ -1,63 +1,44 @@
 """Models to make pose predictions"""
 
-
-import shutil
-import os
 import pandas as pd
 import numpy as np
 import math
+from collections import namedtuple
 
-pd.set_option("display.max_rows", None, "display.max_columns", None)
+Pose = namedtuple("Pose", 'position', 'orientation')
 
 
 class LinearAnalytical:
     """Produces linear regression from the contact/no contact points."""
-    def __init__(self,data):
-        self.data = data
-        self.pos_err,self.ang_err = [],[]
+    def __init__(self):
         self.pil_point_map = {0:[1,1], 1:[1,0], 2:[1,-1], 3:[0,1], 4:[0,0], 5:[0,-1], 6:[-1,1], 7:[-1,0], 8:[-1,-1]}
-        self.drop_cont_indexes = [0,1,2,3,4,5,6,8,9,10,12,13,14,16,17,18,20,21,22,24,25,26,28,29,30,32,33,34,36,37,38,40,41]
-
-    def evaluate(self):
-        """Evaluates the model. Prints results. Since this is an analytical model, all of the data can be used
-        to evaluate"""
-        self.pos_err,self.ang_err = [],[]
-        # For every sample in the data, predict, and add errors.
-        for sample_num in range(len(self.data)):
-            sample = self.data.iloc[[sample_num]]
-            pos,ang = self.predict(sample)
-            if pos is not None:
-                self.pos_err.append(abs((pos - float(sample.position))))
-                self.ang_err.append(abs((ang - float(sample.angle))))
-        self.show_eval_results()
-
-    def show_eval_results(self):
-        """Prints mean error and std deviation for each."""
-        mean_pos_err = np.mean(self.pos_err)
-        std_pos_err = np.std(self.pos_err)
-        mean_ang_err = np.mean(self.ang_err)
-        std_ang_err = np.std(self.ang_err)
-        print("\nPosition Error:\nMean: {}\tStandard Deviation: {}".format(mean_pos_err,std_pos_err))
-        print("\nAngle Error:\nMean: {}\tStandard Deviation: {}".format(mean_ang_err, std_ang_err))
 
 
-    def predict(self,sample):
-        """Returns predicted position and angle."""
-        x_points,y_points = [],[]
-        contacts = sample.drop(sample.columns[self.drop_cont_indexes], axis=1)
-        contacts = contacts.reset_index()
-        contact_list = contacts.loc[0, :].values.tolist()
-        del contact_list[0]
-        for pil_num in range(len(contact_list)):
-            if contact_list[pil_num]:
-                point=self.pil_point_map[pil_num]
-                x_points.append(point[0])
-                y_points.append(point[1])
-        # If not enough points to make a prediction.
-        if len(x_points) <= 2:
-            return None,None
+    def predict(self,sensors):
+        self.poses = []
+        self.contact_list = []
+        for sensor in sensors:
+            for pillar in sensor.pillars:
+                self.contact_list.append(pillar.in_contact)
+            self.contact_to_points()
+            self.points_to_pose()
+        return self.combine_sensor_poses()
+
+    def contact_to_points(self):
+        self.x_points, self.y_points = [], []
+        # Create a list of Booleans for contact/no contact
+        for pil_num in range(len(self.contact_list)):
+            if self.contact_list[pil_num]:
+                point = self.pil_point_map[pil_num]
+                self.x_points.append(point[0])
+                self.y_points.append(point[1])
+
+    def points_to_pose(self):
+        #TODO if this is the second sensor, need to reflect the pillars...
+        if len(self.x_points) <= 2:
+            self.poses.append([None,None])
         # Do linear regression on contact points.
-        z = np.polyfit(x_points, y_points, 1)
+        z = np.polyfit(self.x_points, self.y_points, 1)
         p = np.poly1d(z)
         if len(p.c) == 1: # Slope is zero.
             slope = 0
@@ -67,15 +48,27 @@ class LinearAnalytical:
             intercept = p.c[1]
         ang = math.degrees(math.atan(slope))
         pos = intercept
-        return pos,ang
+        self.poses.append([pos,ang])
 
+    def combine_sensor_poses(self):
+        if self.poses[0].position is None and self.poses[1].position is None:
+            return [None,None]
+        elif self.poses[0].position is None and self.poses[1].position is not None:
+            return [self.poses[1].position,self.poses[1].orientation]
+        elif self.poses[0].position is not None and self.poses[1].position is None:
+            return [self.poses[0].position, self.poses[0].orientation]
+        elif self.poses[0].position is not None and self.poses[1].position is None:
+            return [self.poses[0].position, self.poses[0].orientation]
+        else:
+            return self.average_poses()
 
+    def average_poses(self):
+        position = (self.poses[0].position + self.poses[1].position) / 2
+        orientation = (self.poses[0].orientation + self.poses[1].orientation) / 2
+        return [position,orientation]
 
-def main(data_dir_name):
-    data_path = os.getcwd() + '\\' + 'processed_data' + '\\' + data_dir_name + '\\' + 'labeled_data.csv'
-    data = pd.read_csv(data_path)
-    model = LinearAnalytical(data)
-    model.evaluate()
+def main():
+    pass
 
 if __name__ == '__main__':
-    main("cable_pose_data")
+    main()
