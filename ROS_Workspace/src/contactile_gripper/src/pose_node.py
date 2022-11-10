3133bb04ca4473cd2c5a070d8a76a855f87f823c
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-11-10 13:37:19 -0800

    Update pose_node.py

--- ROS_Workspace/src/contactile_gripper/src/pose_node.py
+++ ROS_Workspace/src/contactile_gripper/src/pose_node.py
@@ -8,16 +8,17 @@ import numpy as np
 from contactile_gripper.msg import Float32List
 from models import ModelType, DataOptions, PoseModel
 from papillarray_ros_v2.msg import SensorState
+from collections import namedtuple
 
 
 # How to pass in arguments with launch file:
 # https://campus-rover.gitbook.io/lab-notebook/faq/using-args-params-roslaunch
 # https://www.folkstalk.com/2022/09/ros-launch-file-with-args-with-code-examples.html
 
-# TODO. If not in contact, publish 999.
+Pose = namedtuple("Pose", "pos ang")
 
 class PoseNode(object):
-    """Takes 3 arguments: model type, data type, saved model path."""
+    """Takes 3 arguments: model type, data type, saved model path. If not in contact, publish 999"""
     def __init__(self):
         rospy.init_node('pose_node', anonymous=False, log_level=rospy.INFO)
         # TODO make sure it finds the right folder.
@@ -39,18 +40,6 @@ class PoseNode(object):
         self.wait_for_data()
         self.main_loop()
 
-    def get_pose_model(self, mt, dt, model_fn):
-        if mt == ModelType.MLPRegressor.name: model_type = ModelType.MLPRegressor
-        elif mt == ModelType.KNeighborsRegressor.name: model_type = ModelType.KNeighborsRegressor
-        elif mt == ModelType.LinearAnalytical.name: model_type = ModelType.LinearAnalytical
-        elif mt == ModelType.LinearRegression.name:model_type = ModelType.LinearRegression
-        else: raise KeyError
-        if dt == DataOptions.ALL.name: data_type = DataOptions.All
-        elif dt == DataOptions.Z_ONLY.name: data_type = DataOptions.Z_ONLY
-        elif dt == DataOptions.CONTACT_ONLY.name:data_type = DataOptions.CONTACT_ONLY
-        else: raise KeyError
-        return PoseModel(model_type, data_type=data_type, model_fn=model_fn)
-
     ######################## Subscriber and service callbacks ########################
     def tact_0_callback(self, msg):
         self.tact_sensor0 = msg
@@ -67,21 +56,16 @@ class PoseNode(object):
         """This is the main loop for the node which executes at self.main_loop_rate."""
         while not rospy.is_shutdown():
             pose = self.determine_pose()
-            pose = self.pose_model.predict([self.tact_sensor0, self.tact_sensor1])
-            if pose.position is None:
-                self.pose_pub.publish([float(0), float(0)])
-            else:
-                self.pose_pub.publish([float(pose.position), float(pose.orientation)])
+            self.pose_pub.publish([float(pose.pos), float(pose.ang)])
             self.main_loop_rate_obj.sleep()
 
-    ######################## Functions ########################
+    ######################## Other ########################
     def determine_pose(self):
-        if not self.in_contact_0 and not self.in_contact_0: return None
-        poses = []
-        poses.append(self.pose_model.model.predict(self.sensor0_pred_data))
-        poses.append(self.pose_model.model.predict(self.sensor1_pred_data))
-
-
+        if not self.in_contact_0 and not self.in_contact_0: return Pose(999, 999)
+        pred0 = self.pose_model.model.predict(self.sensor0_pred_data)
+        pred1 = self.pose_model.model.predict(self.sensor1_pred_data)
+        pose = np.average([pred0[0],pred1[0]],axis=0)
+        return Pose(pose[0], pose[1])
 
     def prep_sensor_data(self, d):
         """Get the data in the form the model expects for prediction."""
@@ -99,6 +83,18 @@ class PoseNode(object):
                 prepped_data += [int(pillar.in_contact)]
         print(prepped_data)
         return np.array([prepped_data])
+
+    def get_pose_model(self, mt, dt, model_fn):
+        if mt == ModelType.MLPRegressor.name: model_type = ModelType.MLPRegressor
+        elif mt == ModelType.KNeighborsRegressor.name: model_type = ModelType.KNeighborsRegressor
+        elif mt == ModelType.LinearAnalytical.name: model_type = ModelType.LinearAnalytical
+        elif mt == ModelType.LinearRegression.name:model_type = ModelType.LinearRegression
+        else: raise KeyError
+        if dt == DataOptions.ALL.name: data_type = DataOptions.All
+        elif dt == DataOptions.Z_ONLY.name: data_type = DataOptions.Z_ONLY
+        elif dt == DataOptions.CONTACT_ONLY.name:data_type = DataOptions.CONTACT_ONLY
+        else: raise KeyError
+        return PoseModel(model_type, data_type=data_type, model_fn=model_fn)
 
     def wait_for_data(self):
         while True:
